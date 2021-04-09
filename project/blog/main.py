da0472c8fbe325a23d10ab882d3e634c0227b012
Zhengguo MacbookPro <zhengguo.gu@gmail.com>
2021-04-09 20:54:18 +0200

    add get method

--- project/blog/main.py
+++ project/blog/main.py
@@ -1,13 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas
 from . import models
-from .database import engine
-
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
 @app.post('/blog')
-def create(request: schemas.Blog): #request is a type of pydantic field Blog
-    return request
+def create(request: schemas.Blog, db: Session = Depends(get_db)): #request is a type of pydantic field Blog
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    return blog
+
+#stopped at 1:43:29
